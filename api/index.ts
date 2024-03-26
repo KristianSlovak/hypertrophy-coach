a92f84f171d9fb67a61446d137e758902ce88f8c
Kristián Slovák <slovak.kristian1@gmail.com>
2024-03-26 08:02:24 +0100

    added database name to connection query

--- api/index.ts
+++ api/index.ts
@@ -27,6 +27,7 @@ app.listen(process.env.SERVER_SIDE_PORT, () => {
 });
 
 const connection = mysql.createConnection({
+  database: process.env.DATABASE,
   host: process.env.DATABASE_HOST,
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
